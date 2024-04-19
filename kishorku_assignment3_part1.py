16b4fef69bff0af7295297840083cc636138b957
KishorKumar37 <d.kishornitt1999@gmail.com>
2024-04-19 17:17:40 -0400

    A3C with N-Step returns

--- kishorku_assignment3_part1.py
+++ kishorku_assignment3_part1.py
@@ -1,240 +1,281 @@
-
 import torch
 from torch import nn
 import gymnasium as gym
-from itertools import count
 from torch import multiprocessing as mp
-import math
-import random
-from tqdm import tqdm
 
-# mp.set_start_method('fork')
-# torch.autograd.set_detect_anomaly(True)
+from tqdm import tqdm
+import optuna
+import numpy as np
+from collections import namedtuple
+
+ENV_NAME        = "CartPole-v1"
+
+env = gym.make(ENV_NAME)
+N_STATES        = env.observation_space.shape[0]
+N_ACTIONS       = env.action_space.n
+del env
+
+N_HIDDEN        = 64
+
+N_WORKERS       = 1
+# N_WORKERS       = mp.cpu_count()
+
+LR              = 1e-2
+DISCOUNT_FACTOR = 0.9
+
+MAX_EPISODES    = 10000
+N_STEP          = 256
+
+DEVICE          = torch.device("cpu")
+DTYPE           = torch.float64
+
+Transition = namedtuple(typename="Transition",
+                        field_names=("policy",
+                                     "action",
+                                     "reward",
+                                     "value_current_state"))
+
+class ReplayBuffer(object):
+
+    def __init__(self):
+        self.buffer = []
+
+    def push(self, *args):
+        self.buffer.append(Transition(*args))
+    
+    def reset(self):
+        self.buffer = []
+
+class SharedAdam(torch.optim.Adam):
+
+    def __init__(self,
+                 params,
+                 lr=LR,
+                 betas=(0.9, 0.999),
+                 eps=1e-8,
+                 weight_decay=0):
+
+        super(SharedAdam).__init__(params,
+                                   lr,
+                                   betas,
+                                   eps,
+                                   weight_decay)
+        
+        for group in self.param_groups:
+            for p in group["params"]:
+                state = self.state[p]
+                state["step"] = torch.zeros(1)
+                state["exp_avg"] = p.data.new().resize_as_(p.data).zero_()
+                state["exp_avg_sq"] = p.data.new().resize_as_(p.data).zero_()
+        
+        self.share_memory()
 
 class ActorCritic(nn.Module):
 
-    def __init__(self,
-                 n_states,
-                 n_actions):
+    def __init__(self):
+
         super().__init__()
         self.shared_fc = nn.Sequential(
-            nn.Linear(n_states, 64),
+            nn.Linear(N_STATES, N_HIDDEN),
             nn.ReLU(),
-            nn.Linear(64, 64),
+            nn.Linear(N_HIDDEN, N_HIDDEN),
             nn.ReLU()
         )
         self.actor = nn.Sequential(
-            nn.Linear(64, n_actions),
+            nn.Linear(N_HIDDEN, N_ACTIONS),
             nn.Softmax(dim=-1)
         )
-        self.critic = nn.Linear(64, 1)
+        self.critic = nn.Linear(N_HIDDEN, 1)
 
     def forward(self, x):
+
         x = self.shared_fc(x)
         return self.actor(x), self.critic(x)
 
-class A3CAgent():
-    def __init__(self,
-                 env_name,
-                 global_model,
-                 num_workers,
-                 device,
-                 lr,
-                 max_episodes,
-                 discount_factor):
-        self.env_name = env_name
-        self.global_model = global_model
-        self.num_workers = num_workers
-        
-        self.lr = lr
-        self.discount_factor = discount_factor
-        self.max_episodes = max_episodes
-        self.device = device
+class A3CMaster():
 
-        # self.episode_rewards = []
-        # self.episode_steps = []
-        # self.episode_losses = []
+    def __init__(self):
 
-        print("Finished initializing of A3C agent")
+        self.global_model = ActorCritic().to(self.device)
+        self.global_mode.share_memory()
+
+        self.global_optimizer = SharedAdam(self.global_model.parameters())
+
+        self.episode_rewards = []
+        self.episode_losses = []
+
+        self.global_reward_queue, self.global_loss_queue = mp.Queue(), mp.Queue
+        self.global_step_counter = mp.Value("i", 0)
     
     def train(self):
-        envs = [gym.make(self.env_name) for _ in range(self.num_workers)]
-        worker_models = [ActorCritic(n_states=envs[0].observation_space.shape[0],
-                                     n_actions=envs[0].action_space.n).to(self.device)
-                                     for _ in range(self.num_workers)]
+
+        workers = [A3CWorker(self.global_model,
+                             self.global_optimizer,
+                             self.global_step_counter,
+                             self.global_reward_queue,
+                             self.global_loss_queue)
+                   for _ in range(N_WORKERS)]
+
+        [worker.start() for worker in workers]
+
+        done = 0
+        while done < N_WORKERS:
+            r = self.global_reward_queue.get()
+            l = self.global_loss_queue.get()
+
+            if r is not None:
+                self.episode_rewards.append(r)
+            else:
+                done += 1
+            
+            if l is not None:
+                self.episode_losses.append(l)
+
+        [worker.join() for worker in workers]
+
+        return self.episode_rewards, self.episode_losses
         
-        for i, (env, worker_model) in enumerate(zip(envs, worker_models)):
-            #  print("Entered multiprocess loop")
-            worker = mp.Process(target=self.train_worker,
-                                args=(env,
-                                      self.global_model,
-                                      worker_model,
-                                      self.lr,
-                                      self.max_episodes,
-                                      i))
-            worker.start()
-            #  print("Executed worker")
-             
+class A3CWorker(mp.Process):
 
-    def train_worker(self,
-                     env,
-                     global_model,
-                     worker_model,
-                     lr,
-                     max_episodes,
-                     worker_name):
-        # print(f"Worker {worker_name} initiated")
-        ''' Kishorkumar Devasenapathy - 04-15-2024
-        # This optimizer is only in the scope of this worker but has a shared copy of the global model's
-        # parameters to perform gradient descent and update
-        # torch.multiprocessing takes care of synchronizing this step across the workers
-        '''
-        optimizer = torch.optim.Adam(global_model.parameters(),
-                                     lr=lr)
-        # print(f"Worker {worker_name} initialized optimizer for global model")
-        # actor_loss_fn = nn.LogSoftmax()
-        # critic_loss_fn = nn.MSELoss()
+    def __init__(self,
+                 global_model,
+                 global_optimizer,
+                 globa_step_counter,
+                 global_reward_queue,
+                 global_loss_queue):
 
-        state, _ = env.reset()
-        state = torch.tensor(state,
-                             dtype=torch.float32,
-                             device=self.device).unsqueeze(0)
-        # print(f"Worker {worker_name} interacted first time with environment")
-        
-        episode_rewards = []
-        episode_steps = []
-        episode_losses = []
+        super(A3CWorker, self).__init__()
 
-        for episode in tqdm(range(max_episodes)):
-            # print(f"Worker {worker_name} started episode {episode}")
-            # optimizer.zero_grad()
-            worker_model.load_state_dict(global_model.state_dict()) # synchronize worker's model with that of global model
-            # print(f"Worker {worker_name} loaded global model's parameters")
+        self.env = gym.make(ENV_NAME)
+        self.local_model = ActorCritic(n_states=self.env.observation_space.shape[0],
+                                       n_actions=self.env.action_space.n).to(self.device)
+        self.local_optimizer = torch.optim.Adam(self.local_model.parameters(),
+                                                lr=LR)
 
-            state, _ = env.reset()
+        self.global_model = global_model
+        self.global_optimizer = global_optimizer
+
+        self.global_step_counter = globa_step_counter
+        self.global_reward_queue = global_reward_queue
+        self.global_loss_queue = global_loss_queue
+
+        self.replay_buffer = ReplayBuffer()
+
+        self.step_counter = 0
+
+    def run(self):
+
+        for episode in tqdm(range(MAX_EPISODES)):
+            self.synchronize_models()
+
+            state, _ = self.env.reset()
             
             episode_reward = 0
             episode_loss = 0
-            # print(state.shape)
 
-            for step in count():
-                # print("Started first step")
-                state_tensor = torch.tensor(state,
-                                 dtype=torch.float32,
-                                 device=self.device)
-                # print("Trying model forward")
-                action_probs, current_state_value = worker_model.forward(state_tensor)
-                # print("Trying model Worked")
-                # print(action_probs.shape)
-                action = torch.max(action_probs,
-                                   dim=-1).indices
-                # print(action.shape)
+            done = False
+
+            start_step = self.step_counter
+
+            while not done:
+                state = torch.tensor(state,
+                                     dtype=DTYPE,
+                                     device=DEVICE)
+                policy, value_current_state = self.local_model.forward(state)
+                action = policy.max(dim=-1).indices
 
                 next_state, reward, terminated, truncated, _ = env.step(action.item())
-                next_state_tensor = torch.tensor(next_state,
-                                          dtype=torch.float32,
-                                          device=self.device)
-                # print("Trying model forward again")
-                _, next_state_value = worker_model.forward(next_state_tensor)
-                # print("Trying model forward again worked")
+
+                self.global_step_counter += 1
+                self.step_counter += 1
+
+                next_state = torch.tensor(next_state,
+                                          dtype=DTYPE,
+                                          device=DEVICE)
 
                 episode_reward = episode_reward + reward
+                reward = torch.tensor(reward,
+                                      dtype=DTYPE,
+                                      device=DEVICE)
 
-                # reward = torch.tensor([reward],
-                #                       dtype=torch.float32,
-                #                       device=self.device)
-                # print(reward.shape)
+                self.replay_buffer.push(policy,
+                                        action,
+                                        reward,
+                                        value_current_state)
 
-                # reward *= 5
-
-                if terminated:
-                    target_current_state_value = reward
-                else:
-                    target_current_state_value = reward + self.discount_factor*next_state_value.item()
-
-                # actor_loss = -torch.log_softmax(action_probs,
-                #                                 dim=-1)
-                # actor_loss = actor_loss_fn(action_probs)
-                # critic_loss = critic_loss_fn(target_current_state_value, current_state_value.item())
-                advantage = target_current_state_value - current_state_value
-
-                actor_loss = -torch.log_softmax(action_probs, dim=-1)[action] * advantage.clone()
-                critic_loss = 0.5 * advantage.pow(2)
-
-                episode_loss = torch.add(episode_loss, (actor_loss + critic_loss))
-                
                 state = next_state
 
-                # optimizer.zero_grad()
-                # print("Trying loss calc")
-                # episode_loss.backward(retain_graph=True)
-                # print("Loss calc worked")
+                done = terminated or truncated
 
-                # print("STarted copying params")
-                # for global_param, worker_param in zip(global_model.parameters(), worker_model.parameters()):
-                #     if global_param.grad is None:
-                #         global_param.grad = worker_param.grad.clone().detach()
-                #     else:
-                #         global_param.grad += global_param.grad + worker_param.grad.clone().detach()
-                # optimizer.step()
-                # print("Finished copying params")
+                if done or (start_step - self.step_counter == N_STEP):
+                    episode_loss += self.accumulate_gradients(terminated,
+                                                              next_state,
+                                                              start_step)
+                    self.asynchronous_update()
+                
+            self.global_reward_queue.put(episode_reward)
+            self.global_loss_queue.put(episode_loss)
+        
+        self.global_reward_queue.put(None)
 
-                if terminated or truncated:
-                    # print(f"Worker {worker_name} completed an episode")
-                    episode_rewards.append(episode_reward)
-                    episode_steps.append(step+1)
-                    episode_losses.append(episode_loss.item())
-                    break
-            optimizer.zero_grad()
-            # print("Trying loss calc")
-            episode_loss.backward(retain_graph=True)
-            # print("Loss calc worked")
+    def accumulate_gradients(self,
+                             terminated,
+                             next_state,
+                             start_step):
+        
+        memory = Transition(*zip(*self.replay_buffer))
 
-            # print("STarted copying params")
-            for global_param, worker_param in zip(global_model.parameters(), worker_model.parameters()):
-                if global_param.grad is None:
-                    global_param.grad = worker_param.grad.clone().detach()
-                else:
-                    global_param.grad += global_param.grad + worker_param.grad.clone().detach()
-            optimizer.step()
-            nn.utils.clip_grad_value_(global_model.parameters(),
-                                      clip_value=500)
-            # print("Finished copying params")
-        # print(f"Worker {worker_name} completed episode {episode}")
+        update_loss = 0
 
-        if worker_name == 0:
-            #  self.episode_rewards = (episode_rewards)
-            #  self.episode_steps = (episode_steps)
-            #  self.episode_losses = (episode_losses)
+        if terminated:
+            R = 0
+        else:
+            _, value_next_state = self.local_model.forward(next_state)
+            R = value_next_state
+        
+        for t in reversed(range(start_step, self.step_counter)):
+            R *= DISCOUNT_FACTOR
+            R += memory.reward[t]
+
+            advantage = R - memory.value_current_state[t]
+            log_policy = -torch.log(memory.policy[t], dim=-1)[memory.action[t]]
+
+            actor_loss = log_policy*advantage
+            critic_loss = 0.5 * torch.square(advantage)
+
+            loss = actor_loss + critic_loss
+            loss.backward()
+            update_loss += loss.item()
+        
+        nn.utils.clip_grad_value_(self.local_model.parameters(),
+                                    clip_value=500)
+        
+        self.replay_buffer.reset()
+
+        return update_loss
+
+    def asynchronous_update(self):
+
+        for global_param, local_param in zip(self.global_model.parameters(), self.local_model.parameters()):
+            if global_param.grad is None:
+                global_param.grad = local_param.grad.clone().detach()
+            else:
+                global_param.grad += global_param.grad + local_param.grad.clone().detach()
+        
+        self.global_optimizer.step()
     
-            torch.save(episode_rewards, f"A3C_cartpole_episode_rewards.pkl")
-            torch.save(episode_steps, f"A3C_cartpole_episode_steps.pkl")
-            torch.save(episode_losses, f"A3C_cartpole_episode_losses.pkl")
-
+    def synchronize_models(self):
+        self.local_optimizer.zero_grad()
+        self.local_model.load_state_dict(self.global_model.state_dict())
            
 if __name__ == "__main__":
-    ENV_NAME        = "CartPole-v1"
-    NUM_WORKERS     = 8
 
-    LR              = 1e-4
-    MAX_EPISODES    = 2000
+    trainer = A3CMaster()
+    episode_rewards, episode_losses = trainer.train()
 
-    DISCOUNT_FACTOR = 0.99
-
-
-    DEVICE = "cpu"
-
-    env = gym.make(ENV_NAME)
-    global_model = ActorCritic(n_states=env.observation_space.shape[0],
-                               n_actions=env.action_space.n).to(DEVICE)
-    global_model.share_memory()
-
-    trainer = A3CAgent(env_name=ENV_NAME,
-                    global_model=global_model,
-                    num_workers=NUM_WORKERS,
-                    device=DEVICE,
-                    lr=LR,
-                    max_episodes=MAX_EPISODES,
-                    discount_factor=DISCOUNT_FACTOR)
-
-    trainer.train()
+    torch.save(episode_rewards,
+               "kishorku_vveera_assignment3_part1_a3c_cartpole_rewards.pkl")
+    torch.save(episode_rewards,
+               "kishorku_vveera_assignment3_part1_a3c_cartpole_losses.pkl")
+    torch.save(trainer.global_model.state_dict(),
+               "kishorku_vveera_assignment3_part1_a3c_cartpole_checkpoint.pkl")
